d91a6080dab8afb2c0592e2623eef138161b7693
je-ferreira <jessicaferreira_adm@live.com>
2020-11-12 15:00:00 -0300

    feat: pagina base login visitante

---
+++ src/pages/Login/LoginVisitante/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginVisitante from '../../../components/Login/LoginVisitante'
+
+const LoginVisitantePage = () => (
+    <LoginVisitante />
+);
+
+export default LoginVisitantePage;
