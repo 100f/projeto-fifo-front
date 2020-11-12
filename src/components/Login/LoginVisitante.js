13564b1f2f57491d7accea2e1acbbc22fd17ba6c
je-ferreira <jessicaferreira_adm@live.com>
2020-11-12 14:58:29 -0300

    feat: componente base login visitante

---
+++ src/components/Login/LoginVisitante.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+import { Button } from '@material-ui/core'
+
+const LoginVisitante = () => {
+    return (
+        <div className="container">
+            <div className="login">
+                <h1 className="login__titulo">ÁREA DE LOGIN</h1>
+                <form>
+                    <div className="login__formulario">
+                        <label htmlFor="nome">Nome</label>
+                        <input id="nome" type="text" name="nome" placeholder="Digite seu nome" />
+                    </div>
+                    <div className="login__formulario">
+                        <label htmlFor="token">Token</label>
+                        <input id="token" type="text" name="token" placeholder="Digite o token recebido" />
+                    </div>
+
+                    <Button type="submit" variant="contained" className="botao-login">Entrar</Button>
+
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginVisitante;
