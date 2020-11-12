f576c81fe2edf1f9875b35353c313d670354e0e0
je-ferreira <jessicaferreira_adm@live.com>
2020-11-12 15:00:35 -0300

    feat: pagina base login funcionario

---
+++ src/pages/Login/LoginFuncionario/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginFuncionario from '../../../components/Login/LoginFuncionario'
+
+const LoginFuncionarioPage = () => (
+    <LoginFuncionario />
+);
+
+export default LoginFuncionarioPage;
