069f170833f2a95210e2444d1812506da4828d3a
Caio Enrique <cemf1996@gmail.com>
2020-11-17 16:31:45 -0300

    feat: criada a estrutura e estilização básicas da página Home para testes

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <div id='page-home'>
+      <h2>O que estão jogando agora</h2>
+    </div>
+  )
+};
+
+export default Home;
